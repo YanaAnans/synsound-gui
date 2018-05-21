503743f60c2cbf38b18753c9111e962d68a22104
YanaAnans <32519578+YanaAnans@users.noreply.github.com>
2018-05-21 22:57:06 +0700

    Change latin text to cyrillic

--- src/v1/Controller.java
+++ src/v1/Controller.java
@@ -6,6 +6,8 @@ import java.util.ResourceBundle;
 import com.yananas.synsound.AudioEditor;
 import com.yananas.synsound.AudioLibrary;
 import com.yananas.synsound.AudioPlayer;
+import com.yananas.synsound.experiments.Hyphenator;
+import com.yananas.synsound.experiments.TextTransformer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,16 +25,23 @@ public class Controller implements Initializable {
 	private TextArea text;
 
 	@FXML
+	private TextArea samples;
+
+	@FXML
 	private ChoiceBox<String> examples;
 
 	public void play(ActionEvent event) {
-		String phonemes = text.getText();
-		AudioPlayer.play(AudioEditor.union(AudioLibrary.phonemes(phonemes)));
+		TextTransformer tt = new TextTransformer();
+		String cyrillicText = text.getText();
+		cyrillicText = Hyphenator.textToSamples(cyrillicText);
+		cyrillicText = tt.transform(cyrillicText);
+		samples.setText(cyrillicText);
+		AudioPlayer.play(AudioEditor.union(AudioLibrary.phonemes(cyrillicText)));
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		text.setText("-p r'i v'e et- -m'i ir-");
+		text.setText("Привет мир");
 	}
 
 }
